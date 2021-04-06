55555207f478afea48e38375b4865f99d9dfdc26
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2021-04-06 19:25:40 +0200

    Added GoldenApple script and prefab

---
+++ Assets/Scripts/GoldenApple.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldenAppleController : MonoBehaviour
+{
+    private int healingPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        healingPoints = 40;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if Collided with player
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().gainSanity(healingPoints);
+            Destroy(gameObject);
+        }
+    }
+
+}
