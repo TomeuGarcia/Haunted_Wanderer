978064c9421b58385ced2e363113e6a0f96514dc
Jamie <jamiealexander.lobban@enti.cat>
2021-04-08 09:30:33 +0200

    Added HealthBar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxSanity(int sanity)
+    {
+        slider.maxValue = sanity;
+        slider.value = sanity;
+    }
+
+    public void SetHealth(int sanity)
+    {
+        slider.value = sanity;
+    }
+}
