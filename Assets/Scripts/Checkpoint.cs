3633c7b161bfe4d9b21e555dbc442c6ea9d6cc40
Tomeu Garcia Pintó <tomeu.garcia@enti.cat>
2021-05-01 19:01:47 +0200

    Added Checkpoint script, added Checkpoint prefab

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+
+    [Header("Checkpoint respawn coords:")]
+    [SerializeField] public float X;
+    [SerializeField] public float Y;
+    [SerializeField] public float Z;
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
